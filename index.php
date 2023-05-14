693acc9787dac32a151e6000708cc3923b25ec23
PI4entrega2 <133603187+PI4entrega2@users.noreply.github.com>
2023-05-14 17:01:25 -0300

    index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="pt-br">
+
+    <head>
+        <meta charset="UTF-8">
+
+        
+        <title>Saúde Financeira</title>
+
+        
+        <link href="index.css" rel="stylesheet" />
+    </head>
+
+    <body>
+        
+        <div class="container">
+            
+            <div class="title"><h1>Saúde Financeira</h1></div>
+            
+            <div class="inner-container">
+                <img src="image1.png">
+    
+                <div class="texts">
+                    <div class="text">Nesta plataforma, você poderá verificar em quanto tempo você alcançará uma certa quantia em dinheiro, utilizando alguns tipos de investimento.</div>
+
+                    <div class="text">É só indicar quanto você quer investir inicialmente e qual será o aporte mensal.</div>
+                </div>
+
+            </div>
+            
+            <a href="questionario.php">Clique aqui para começar</a>
+
+        </div>
+
+    </body>
+
+</html>
